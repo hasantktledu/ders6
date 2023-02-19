1afcbd016f16c87f9075173cc3a2e36dd5ea677d
Hasan Tokatlı <hasantktl.edu@gmail.com>
2023-02-19 20:35:43 +0300

    feat: promise örneği yapıldı

---
+++ script.js
@@ -0,0 +1,20 @@
+let prObje = new Promise( 
+    (a, b) => {
+        //..... zaman alan işlemler burada kodlanır
+        for ( let i = 0; i<50000; i++ ) {
+            console.log(i)
+        }
+
+        a("OK")
+    }  
+)
+
+prObje.then( (deger)=>{ console.log("SMS gönderimi tamamlandı" + deger) } )
+
+prObje.catch( (hata) => { console.log(hata)  } )
+//prObje.then( (deger)=>{ console.log("Mesaj 2" + deger) } )
+
+
+//Promiseler içerisinde süre alan kodlar barındırır. İşlemler tamamlanınca negatrif veya pozitif bir sonuc döndürürler..
+
+//Programcılar, promiseleri dinleyerek, promiseler işini bitirince farklı işlemler yaptırmak üzere kodlarını yazabilir...
